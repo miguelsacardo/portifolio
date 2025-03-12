2a101ea089b32a05805f5a02fb241f854a7f2d3d
miguelsacardo <miguelsacardo3@gmail.com>
2025-03-12 14:13:27 -0300

    adding comments to the source code 12/03/2025

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,31 @@ async function loadGitRepositories() {
 
   try {
     const response = await fetch(
-        "https://api.github.com/users/miguelsacardo/repos"
-      );
-    
-      // throw a  window alert if the repositories don't load
-      if (!response.ok)
-        window.alert("Falha ao carrergar os repositórios do github.");
-    
-      const repositories = await response.json();
-      repositories.forEach((repository) => {
+      "https://api.github.com/users/miguelsacardo/repos"
+    );
 
-        // if repository description == null,
-        //  put a declaration text for better understanding
-        if(repository.description == null){
-            repository.description = "O repositório não tem descrição.";
-        }
-        const html = `<div class="card-repository">
-                        <h3><a href="https://github.com/miguelsacardo/${repository.name}" target="_blank">${repository.name.toUpperCase()}</a></h3> 
+    // throw a  window alert if the repositories don't load
+    if (!response.ok)
+      window.alert("Falha ao carrergar os repositórios do github.");
+
+    const repositories = await response.json();
+    repositories.forEach((repository) => {
+      // if repository description == null,
+      //  put a declaration text for better understanding
+      if (repository.description == null) {
+        repository.description = "O repositório não tem descrição.";
+      }
+      const html = `<div class="card-repository">
+                        <h3><a href="https://github.com/miguelsacardo/${
+                          repository.name
+                        }" target="_blank">${repository.name.toUpperCase()}</a></h3> 
                         <p>${repository.description}</p>        
                         </div>`;
-        listRepositories.innerHTML += html;
-      });  
+      listRepositories.innerHTML += html;
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
 }
 
 loadGitRepositories();
